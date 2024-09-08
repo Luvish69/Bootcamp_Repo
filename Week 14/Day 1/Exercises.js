50bfa0e1bd675a9088d91b6faefc5af0e6a075b3
Luvish69 <luvishlakhan@gmail.com>
2024-09-08 15:46:51 +0400

    new

---
+++ Week 14/Day 1/Exercises.js
@@ -0,0 +1,6 @@
+let favouritefood = "Chicken";
+let favouritemeal = "Dinner";
+
+console.log(`I eat ${favouritefood} at every ${favouritemeal}`);
+
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
