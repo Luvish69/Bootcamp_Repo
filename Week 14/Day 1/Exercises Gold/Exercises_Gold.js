92f16a120d841df68d79b50724f6484ddf546d63
Luvish69 <luvishlakhan@gmail.com>
2024-09-22 18:33:29 +0400

    new

---
+++ Week 14/Day 1/Exercises Gold/Exercises_Gold.js
@@ -0,0 +1,10 @@
+// Exercise 1
+let sentence = ["my","favorite","color","is","blue"];
+let sentencephrase = sentence.join(" ");
+
+console.log(sentencephrase);
+
+// Exercise 2
+let str1 = "mix";
+let str2 = "pod";
+
